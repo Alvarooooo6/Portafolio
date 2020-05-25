fcb17f3db7d1f1d7d9f454a68e768d2c3088e108
Alvaro Carrizosa <carrizosaralvaro@gmail.com>
2020-05-24 22:20:09 -0500

    Fin Seccion 4

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../app/interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
-    info: any={};
+    info: InfoPagina ={};
     cargada = false;
 
   constructor(private http: HttpClient) { 
@@ -14,7 +15,7 @@ export class InfoPaginaService {
 
       //Leer archivo JSON
       this.http.get('assets/data/data-pagina.json')
-          .subscribe( resp => { 
+          .subscribe( (resp:InfoPagina) => { 
             this.cargada=true;
             this.info=resp;
 
